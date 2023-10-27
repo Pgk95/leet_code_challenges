29b4f99dcc2e9d2293ef96c0d9f6c64318bce3a7
pgk95 <kalupgk@gmail.com>
2023-10-27 14:11:56 +0300

    yet to be finished

---
+++ Two_sum/sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums = [1, 2, 3, 4,]
+        target = int
+
+        for target in nums:
+            return nums[0] + nums[1]
+
+        print(target)
